2f9a52a32825c1fd89c5b415e53b715960b5ccc2
hephaestus <wangxuan8901@126.com>
2018-12-20 16:48:07 +0800

    PlusOne pass

--- src/com/general/NextGreaterElementI.java
+++ src/com/general/NextGreaterElementI.java
@@ -1,7 +1,6 @@
 package com.general;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 496. Next Greater Element I
@@ -10,25 +9,35 @@ public class NextGreaterElementI {
     public static void main(String[] args) {
         int[] num1 = {4,1,2}, num2 = {1,3,4,2};
         int[] nums1 = {2,4}, nums2 = {1,2,3,4};
-        printArr(nextGreaterElement(num1,num2));
-//        printArr(nextGreaterElement(nums1,nums2));
+//        printArr(nextGreaterElement(num1,num2));
+        printArr(nextGreaterElement(nums1,nums2));
     }
 
     private static int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int length = nums1.length;
         int length2 = nums2.length;
         int[] newArr  =new int[nums1.length];
-        for(int i=0;i<length;i++){
-            int each = nums1[i];
-            boolean flag = false;
-            for(int j=i;j<length2;j++){
-                if(nums2[j]>each){
-                    newArr[i] = nums2[j];
-                    flag = true;
-                    break;
-                }
+        List<Integer> list = new ArrayList<Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<length2; i++){
+            int each = nums2[i];
+            list.add(each);
+            map.put(each, i);
+        }
+        for(int j=0;j<length; j++){
+            int each = nums1[j];
+            int pointer = map.get(each);
+            int value;
+            if(pointer==list.size()-1){
+                value = -1;
+            }else{
+                value = list.get(pointer+1);
             }
-            if (!flag) newArr[i] = -1;
+            if(value > each ){
+                newArr[j] = value;
+            }else{
+                newArr[j] = -1;
+            }
         }
         return newArr;
     }
