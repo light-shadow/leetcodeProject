8c20f917704a6c7de93d30ed3bb3fd869e6f6a6b
hephaestus <wangxuan8901@126.com>
2019-03-06 14:26:48 +0800

    src/com/general/algorithm/BinaryTreePaths.java init

---
+++ src/com/general/algorithm/BinaryTreePaths.java
@@ -0,0 +1,18 @@
+package com.general.algorithm;
+
+import java.util.List;
+
+/**
+ * 257. Binary Tree Paths
+ * status:
+ */
+public class BinaryTreePaths {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+
+    }
+}
