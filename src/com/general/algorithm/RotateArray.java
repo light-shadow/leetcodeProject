2ad11366471780ed723a966db4d1cdc2eb048f5a
hephaestus <wangxuan8901@126.com>
2019-03-11 14:58:27 +0800

    reference solution pass 189. Rotate Array

--- src/com/general/algorithm/RotateArray.java
+++ src/com/general/algorithm/RotateArray.java
@@ -3,6 +3,13 @@ package com.general.algorithm;
 /**
  * 189. Rotate Array
  * status:todo
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
  */
 public class RotateArray {
 
@@ -10,23 +17,18 @@ public class RotateArray {
         int[] arrs1 = { 1,2,3,4,5,6,7};
 
         rotate(arrs1, 3);
+        printArr(arrs1);
     }
 
     public static void rotate(int[] nums, int k) {
-        for(int i=0;i<k;i++){
-            int last = nums[nums.length - 1];
-            int first = nums[0];
-            for(int j= nums.length-1;j>=1;j--){
-                if(j-1 ==0){
-
-                }
-                nums[j] = nums[j-1];
-
-
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
             }
-        }
-        for(int each: nums){
-            System.out.println(each);
         }
     }
 
