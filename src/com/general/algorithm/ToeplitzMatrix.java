76ad8dc93aeb58d17cc6c42d31b39b753bf9fa7e
hephaestus <wangxuan8901@126.com>
2019-03-19 15:42:56 +0800

    pass 766. Toeplitz Matrix

--- src/com/general/algorithm/ToeplitzMatrix.java
+++ src/com/general/algorithm/ToeplitzMatrix.java
@@ -3,21 +3,29 @@ package com.general.algorithm;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 766. Toeplitz Matrix
+ * status: pass
+ */
 public class ToeplitzMatrix {
     public static void main(String[] args) {
 
     }
-    // TODO 暂未完成
-    private static boolean isToeplitzMatrix(int[][] matrix) {
-        int firstLength = matrix.length;
-        List<String> list = new ArrayList<String>();
-        for(int i=0;i<firstLength;i++){
-            int[] each = matrix[i];
-            int length = each.length;
-            for(int j=0;j<length;j++){
 
+    private static boolean isToeplitzMatrix(int[][] matrix) {
+        int length1 = matrix.length;
+        if(length1 == 1){
+            return true;
+        }
+        for(int i=1;i<length1;i++){
+            int[] newArr = matrix[i];
+            int length2 = newArr.length;
+            for(int j=1;j<length2;j++){
+                if(newArr[j] != matrix[i-1][j-1]){
+                   return false;
+                }
             }
         }
-        return false;
+        return true;
     }
 }
