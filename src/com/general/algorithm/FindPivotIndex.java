47983e3c7e667884ca8b0ac8901672fe86a710c7
hephaestus <wangxuan8901@126.com>
2019-03-21 13:59:18 +0800

    pass 724. Find Pivot Index

--- src/com/general/algorithm/FindPivotIndex.java
+++ src/com/general/algorithm/FindPivotIndex.java
@@ -1,9 +1,9 @@
 package com.general.algorithm;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+/**
+ * 724. Find Pivot Index
+ * status: pass
+ */
 public class FindPivotIndex {
     public static void main(String[] args) {
         int[] arr= new int[]{1, 7, 3, 6, 5, 6};
@@ -11,23 +11,39 @@ public class FindPivotIndex {
     }
     private static int pivotIndex(int[] nums) {
         int length = nums.length;
-        int[] newArr = new int[length+1];
-        for(int i=0;i<length+1;i++){
-            if(i==0){}
+        if(length < 3){
+            return -1;
+        }
+        for(int i=0;i<length;i++){
+            int left;
+            if(i==0){
+                left = 0;
+            }
             else{
-                newArr[i] = nums[length-i];
+                left  = getSum(0, i-1, nums);
+            }
+            int right  = getSum(i+1, length-1, nums);
+            if((left == right)){
+                System.out.println(nums[i]);
+                return i;
             }
         }
-        for (int each: newArr) {
-            System.out.println(each);
+        return -1;
+    }
+
+    /**
+     * 根据参数计算数组的元素之和
+     * 包含开始，包含结束
+     * @param start
+     * @param end
+     * @param nums
+     * @return
+     */
+    public static int getSum(int start, int end, int[] nums){
+        int count = 0;
+        for(int i=start;i<=end;i++){
+            count+=nums[i];
         }
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i=1;i<length;i++){
-            if(nums[i]==newArr[i]){
-                list.add(i);
-            }
-        }
-        System.out.println("----------------");
-        return Collections.min(list);
+        return count;
     }
 }
